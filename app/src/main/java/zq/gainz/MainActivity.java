8353449dddc18888cbd93c245cb56030eddcf332
ZQ <zquinn@rightpoint.com>
2017-11-07 15:37:52 -0600

    Modified MainActivity Added TestAddFile.java

--- app/src/main/java/zq/gainz/MainActivity.java
+++ app/src/main/java/zq/gainz/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void TestUpload() {
         int i = 1 + 1;
+        int ii = 2 + 2;
+        int iii = i + ii;
     }
 
 }
---
+++ app/src/main/java/zq/gainz/TestAddFile.java
@@ -0,0 +1,8 @@
+package zq.gainz;
+
+/**
+ * Created by zquinn on 11/7/2017.
+ */
+
+public class TestAddFile {
+}
