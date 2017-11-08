ace60e0f02c46d207d840ed16a2e7d9cacbf8f48
ZQ <zquinn@rightpoint.com>
2017-11-08 10:51:20 -0600

    setting up with Azure DB backend

--- app/src/main/java/zq/gainz/MainActivity.java
+++ app/src/main/java/zq/gainz/MainActivity.java
@@ -2,13 +2,43 @@ package zq.gainz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.microsoft.windowsazure.mobileservices.*;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MobileServiceClient mClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            mClient = new MobileServiceClient(
+                    "https://zq-gainz.azurewebsites.net",
+                    this
+            );
+        }
+        catch (java.net.MalformedURLException ex) {
+            ex.printStackTrace();
+        }
+
+
+        final TodoItem item = new TodoItem();
+        item.Text = "Awesome item";
+        mClient.getTable(TodoItem.class).insert(item, new TableOperationCallback<TodoItem>() {
+            public void onCompleted(TodoItem entity, Exception exception, ServiceFilterResponse response) {
+                if (exception == null) {
+                    // Insert succeeded
+                } else {
+                    // Insert failed
+                }
+            }
+        });
+
     }
 
     private void TestUpload() {
---
+++ app/src/main/java/zq/gainz/TodoItem.java
@@ -0,0 +1,10 @@
+package zq.gainz;
+
+/**
+ * Created by zquinn on 11/7/2017.
+ */
+
+public class TodoItem {
+    public String Id;
+    public String Text;
+}
